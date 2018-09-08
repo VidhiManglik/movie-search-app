42061eb29524c50ddee8a6c6bc96d8ec28fe11ab
Vidhi Manglik <manglik.vidhi@gmail.com>
2018-09-09 00:29:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+    res.render("search");
+});
+
+app.get('/results',function(req,res){
+    // var str = 'http://starlord.hackerearth.com/movieslisting/';
+    var str = 'http://www.omdbapi.com/?apikey=b1c7b347&s=' + req.query.search;
+    request(str,function(error,response,body){
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            // console.log(results);
+            res.render('results',{data : results});
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" ,function(){
+    console.log("Server On");
+});
