74e968988f07764396940f1133aa62dbd56e53d9
Vidhi Manglik <manglik.vidhi@gmail.com>
2018-09-09 01:10:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.get('/results',function(req,res){
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" ,function(){
-    console.log("Server On");
+    console.log("Server Started On localhost:3000");
 });
